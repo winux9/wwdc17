9f477421cffb0fa109a5acf0303ab004397a7968
przemooo88 <p.lukanowski@wp.pl>
2016-09-27 15:11:18 +0200

    Save my work

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+
+
+    $('document').ready(function() {
+     
+  $("#owl-example").owlCarousel({
+      center: true,
+      autoplay:true,
+      autoPlay: 3000, 
+      autoplayHoverPause: true,
+      margin:10,
+      loop:true,
+      dots: true,
+      pagination: true,
+      autoWidth:true,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+        
+    
+    });
+
